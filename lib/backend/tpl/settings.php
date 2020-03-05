25dfaba4e8c94b38f956812225fc9d7a2f206775
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-03-05 14:33:31 +0100

    Removed run_type() from setting calls.

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -2,9 +2,9 @@
 	<div class="sv_section_description"><?php echo $module->get_section_desc(); ?></div>
     <div class="sv_setting_flex">
 		<?php
-			echo $module->get_setting( 'border_top_color' )->run_type()->form();
-			echo $module->get_setting( 'border_bottom_color' )->run_type()->form();
-			echo $module->get_setting( 'margin' )->run_type()->form();
+			echo $module->get_setting( 'border_top_color' )->form();
+			echo $module->get_setting( 'border_bottom_color' )->form();
+			echo $module->get_setting( 'margin' )->form();
 		?>
     </div>
 <?php } ?>
