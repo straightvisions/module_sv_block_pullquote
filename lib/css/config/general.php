52140fc9222fc615544119429f07019012c24be7
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-10-26 13:35:45 +0100

    FIX SETTINGS & CSS

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -2,12 +2,18 @@
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-pullquote' : '.sv100_sv_content_wrapper article .wp-block-pullquote',
 		array_merge(
-			$module->get_setting('font')->get_css_data('font-family'),
-			$module->get_setting('font_size')->get_css_data('font-size','','px'),
-			$module->get_setting('line_height')->get_css_data('line-height'),
-			$module->get_setting('text_color')->get_css_data(),
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
 			$module->get_setting('border')->get_css_data()
+		)
+	);
+
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-pullquote p' : '.sv100_sv_content_wrapper article .wp-block-pullquote p',
+		array_merge(
+			$module->get_setting('font')->get_css_data('font-family'),
+			$module->get_setting('font_size')->get_css_data('font-size','','px'),
+			$module->get_setting('line_height')->get_css_data('line-height'),
+			$module->get_setting('text_color')->get_css_data()
 		)
 	);
